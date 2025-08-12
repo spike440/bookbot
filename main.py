e6fa957af89b2f56c6396e7255035ad87e764eed
spike440 <mitchelljones186@gmail.com>
2025-08-12 11:26:21 +0800

    possibly completed project

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from stats import character_count
+from stats import print_report
+import sys
 
-def main():
-    print(character_count())
+# Check if the user provided a file path as an argument
+# If not, print usage instructions and exit
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    filepath = sys.argv[1]
+    print_report(filepath)
 
-main()
+
+
+
--- stats.py
+++ stats.py
@@ -1,16 +1,18 @@
-def count_words():
-    text = get_book_text("./books/frankenstein.txt")
-    words = text.split()
-    return len(words)
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
-def character_count():
+def count_words(document_path):
+    text = get_book_text(document_path)
+    words = text.split()
+    return len(words)
+
+def character_count(document_path):
     answer = {}
-    text = get_book_text("./books/frankenstein.txt").lower()
+    text = get_book_text(document_path).lower()
     for char in text:
         if char in answer:
             answer[char] += 1
@@ -18,4 +20,27 @@ def character_count():
             answer[char] = 1
     return answer
 
-print(character_count())
+def chars_dict_to_list(character_dict):
+    sorted_list = []
+    for ch in character_dict:
+        sorted_list.append({"char":ch,"num": character_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def sort_on(d):
+    return d["num"]
+
+def print_report(document_path):
+    test = character_count(document_path)
+    sorted_list = (chars_dict_to_list(test))
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at", document_path)
+    print("----------- Word Count ----------")
+    print("Found", count_words(document_path), "total words")
+    print("--------- Character Count -------")
+    for i in range(0,len(sorted_list)):
+        # Only print alphabetic characters
+        if sorted_list[i]["char"].isalpha() == True:
+           print (f"{sorted_list[i]["char"]}: {sorted_list[i]["num"]}")
+    print("============= END ===============")
